8bdf665376bae0b9dc1e731cf84f6f78a2f12ee2
Sanish <sanish.kamboj@gmail.com>
2021-01-12 10:50:25 +0530

    new changes

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,16 @@ module.exports = {
     runtimeCompiler: true,
     devServer: {
       proxy: 'http://lee.vectorcontrolsystem.com'
-  },
+    },
     configureWebpack: {
       //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
       resolve: {
         symlinks: false
       }
     },
+    pwa: {
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+    }
     transpileDependencies: ['@coreui/utils']
   }
